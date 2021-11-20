09adf53e6f78223187766d09c97590df70e7f210
Zach <>
2021-11-21 01:54:38 +1100

    Fix the pluralize format

--- app/decorators/quote_product_decorator.rb
+++ app/decorators/quote_product_decorator.rb
@@ -2,8 +2,10 @@ class QuoteProductDecorator < ApplicationDecorator
   delegate_all
 
   def name
-    if name = blank_blue_ray_disk?
+    if blank_blue_ray_disk? && amount == 1
       "Blank Blue-Ray disk"
+    elsif blank_blue_ray_disk?
+      "Blank Blue-Ray disks"
     elsif amount == 1
       product.try(:humanize)
     else
